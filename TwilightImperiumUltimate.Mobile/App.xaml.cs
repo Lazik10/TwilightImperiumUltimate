6033a6ef3960d8ecc7c94e3c6ed3e4360d02916e
Lázik <LazikL@email.cz>
2023-06-18 11:04:37 +0200

    Infra\Added static code analyzers and .editconfig

--- TwilightImperiumUltimate.Mobile/App.xaml.cs
+++ TwilightImperiumUltimate.Mobile/App.xaml.cs
@@ -1,12 +1,11 @@
-﻿namespace TwilightImperium.Mobile
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿namespace TwilightImperium.Mobile;
 
-            MainPage = new AppShell();
-        }
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
     }
 }
