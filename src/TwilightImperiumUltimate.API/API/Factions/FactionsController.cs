62d3fff10fff4690ac5a8b16dd2ecaf2bd040902
Lázik <LazikL@email.cz>
2023-08-24 23:59:04 +0200

    API\Basic Factions API

---
+++ src/TwilightImperiumUltimate.API/API/Factions/FactionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwilightImperiumUltimate.API.DTOs.Factions;
+using TwilightImperiumUltimate.DataAccess.DbContexts.TwilightImperium;
+
+namespace TwilightImperiumUltimate.API.API.Factions;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FactionsController : ControllerBase
+{
+    private readonly IDbContextFactory<TwilightImperiumDbContext> _context;
+
+    public FactionsController(IDbContextFactory<TwilightImperiumDbContext> context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<FactionDto>>> GetAllFactionsInfo()
+    {
+        using var context = _context.CreateDbContext();
+
+        var factions = await context.Factions
+            .ToListAsync()
+            .ConfigureAwait(false);
+
+        var factionDtos = factions
+            .Select(x => new FactionDto
+            {
+                FactionName = x.FactionName,
+                Commodities = x.Commodities,
+                ComplexityRating = (int)++x.ComplexityRating,
+                GameVersion = x.GameVersion,
+            })
+            .ToList();
+
+        return factionDtos;
+    }
+}
---
+++ src/TwilightImperiumUltimate.API/DTOs/Factions/FactionDto.cs
@@ -0,0 +1,15 @@
+﻿using TwilightImperiumUltimate.Core.Enums.Game;
+using TwilightImperiumUltimate.Core.Enums.Races;
+
+namespace TwilightImperiumUltimate.API.DTOs.Factions;
+
+public class FactionDto
+{
+        public FactionName FactionName { get; set; }
+
+        public int Commodities { get; set; }
+
+        public int ComplexityRating { get; set; }
+
+        public GameVersion GameVersion { get; set; }
+}
